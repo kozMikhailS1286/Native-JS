027050cc76a652ac8bc80c7f8320202c108bc2cb
Mikhail <109968626+kozMikhailS1286@users.noreply.github.com>
2023-07-19 23:29:40 +0300

    Add files via upload

--- js13.js
+++ js13.js
@@ -8,17 +8,38 @@
 // "use strict";
 
 // console.log(this);
+// Указывает на глобальный объект window
+
+
+
+
 
 // function foo() {
+//     console.log(this);
+//     // У стрелочной функции её this определяется в момент создания этой ф-ии
+//     // и берется из первой внешней области видимости до этой ф-ии.
+// }
+// foo();
+
+
+
+
+
+// function foo() {
+//   // console.log(this);
 //   const foo1 = () => {
 //     console.log(this);
+//     // У стрелочной функции её this определяется в момент создания этой ф-ии
+//     // и берется из первой внешней области видимости до этой ф-ии.
+//     // в этом случае она ищет ее до foo1, там тот же глобальный объект window.
 //   };
 //   foo1();
 // }
-
 // foo();
 
-// var maxSpeed = 180;
+
+
+
 
 // const car = {
 //   maxSpeed: 200,
@@ -28,24 +49,27 @@
 //     const foo = () => {
 //       console.log(this.maxSpeed);
 //     };
-//     foo(); //window ????
+//     foo();
 //   },
 // };
-
 // car.showMaxSpeed();
 
+
+
+
+
 // function foo() {
-//   // console.log(this);
-//   //this
 //   function foo1() {
 //     console.log(this);
 //   }
 //   foo1();
 // }
-
 // const bar = foo;
-
 // bar();
+
+
+
+
 
 // const car1 = {
 //   maxSpeed: 180,
@@ -53,20 +77,35 @@
 //     console.log(this.maxSpeed);
 //   },
 // };
-
+//
 // const car2 = {
 //   maxSpeed: 200,
 // };
-
+//
 // const car3 = {
 //   maxSpeed: 300,
 // };
 
-// // car1.showMaxSpeed.bind(car2)();
+// car1.showMaxSpeed.call(car2);
+// call вызывает вместо this - car2 (перед maxSpeed в console.log)
+
+// car1.showMaxSpeed.apply(car2);
+// apply работает так же как и call.
+
+// car1.showMaxSpeed.bind(car2);
+// функция обёртка.
+
+// const foo = car1.showMaxSpeed.bind(car2);
+// foo()
+// но если мы ее сохраним в переменную, то уже будет отображение.
 
 // const b = car1.showMaxSpeed;
-
 // b();
+// undefined потому что подставляется window.b() и терятеся контекст вызова.
+
+
+
+
 
 // function Car(maxSpeed, color) {
 //   // {}
@@ -74,18 +113,22 @@
 //   this.color = color;
 //   // return this
 // }
-
+//
 // Car.prototype.showMaxSpeed = function () {
 //   console.log(this.maxSpeed);
 // };
-
+//
 // const car1 = new Car(200, "red");
 // const car2 = new Car(180, "black");
-
+//
 // car1.showMaxSpeed();
+//
+// console.log(car1);
+// console.log(car2);
 
-// // console.log(car1);
-// // console.log(car2);
+
+
+
 
 // class Car2 {
 //   constructor(maxSpeed, color) {
@@ -154,15 +197,15 @@
 // car.secondShowColor();
 // car.thirdShowColor();
 
-const car = { color: "red" };
-
-function foo() {
-  console.log(this.color);
-}
-
-const bindedFoo = foo.bind(car);
-
-bindedFoo();
+// const car = { color: "red" };
+//
+// function foo() {
+//   console.log(this.color);
+// }
+//
+// const bindedFoo = foo.bind(car);
+//
+// bindedFoo();
 
 // function foo() {
 //   console.log(this);
