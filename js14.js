8206f14574d9cb4139b451fd2986b9efc19f95c6
Mikhail <109968626+kozMikhailS1286@users.noreply.github.com>
2023-09-29 15:46:16 +0300

    Add files via upload

---
+++ js14.js
@@ -0,0 +1,142 @@
+// js14.
+
+
+// 1:
+
+// const car1 = {
+//     brand: "BMV",
+//     speed: 200,
+//     stareEngine() {
+//         console.log(`Start ${this.brand}`)
+//     }
+// }
+//
+// const car2 = {
+//     brand: "Kia",
+//     speed: 220,
+//     stareEngine() {
+//         console.log(`Start ${this.brand}`)
+//     }
+// }
+//
+// car2.stareEngine() // Start Kia
+//
+// function createCar (brand, speed) {
+//     return {
+//         brand,
+//         speed,
+//         stareEngine() {
+//             console.log(`Start ${this.brand}`)
+//         }
+//     }
+// }
+//
+// const car3 = createCar("Audi", 240)
+// const car4 = createCar("Toyota", 250)
+// console.log(car3)
+// console.log(car4)
+
+
+
+
+// 2:
+
+// function Car(brand, speed) {
+//     this.brand = brand
+//     this.speed = speed
+//     // this.startEngine = function() {
+//     //     console.log(`Start ${this.brand}`)
+//     // }
+//     //return {}
+// }
+//
+// Car.prototype.startEngine = function () {
+//     console.log(`Start ${this.brand}`)
+// }
+//
+//
+// const bmv = new Car('Bmv', 200);
+// const kia = new Car('Kia', 220);
+//
+// // bmv.startEngine;
+// // kia.startEngine;
+//
+// console.log(bmv)
+// console.log(kia)
+//
+// bmv.startEngine()
+
+
+
+
+
+// 3:
+
+// class Car {
+//
+//     constructor(brand, speed) {
+//         this.brand = brand
+//         this.speed = speed
+//     }
+//     startEngine() {
+//         console.log(`Start ${this.brand}`) // обычную ф-ию наследуюь только кто использует.
+//     }
+//     stopEngine = () => {
+//         console.log(`Stop ${this.brand}`) // стрелочную ф-ию унаследуют все.
+//     }
+// }
+//
+// const bmv = new Car('Bmv', 200);
+// const kia = new Car('Kia', 220);
+//
+// console.log(bmv)
+// console.log(kia)
+//
+// bmv.startEngine()
+// // bmv.stopEngine()
+
+
+
+
+
+// 4:
+
+class Car {
+    #brand // скрывает свойство brand
+    constructor(brand, speed) {
+        this.#brand = brand
+        this.speed = speed
+    }
+    startEngine() {
+        console.log(`Start ${this.brand}`) // обычную ф-ию наследуюь только кто использует.
+    }
+    getBrand() {
+        return this.#brand.toUpperCase()
+    }
+    setBrand(newBrand) {
+        if (newBrand.length < 4) throw new Error("Short name")
+        this.#brand = newBrand
+    }
+    get brand() {
+        return this.#brand
+    }
+    set brand(newBrand) {
+        this.#brand = newBrand
+    }
+}
+
+const bmv = new Car('Bmv', 200);
+const kia = new Car('Kia', 220);
+
+// console.log(bmv)
+// console.log(kia)
+
+// bmv.setBrand("Mazda") // будет ожидаемая ошибка если длина меньше 4.
+
+// console.log(bmv.getBrand())
+// console.log(kia.getBrand())
+
+console.log(bmv.brand)
+
+bmv.brand = "ToYoTa"; // Если после brand стоит равно, значит set. Если не стоит равно, значит get.
+console.log(bmv.brand)
